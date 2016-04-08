3cc2e168b429a258d7aa492a64677dc7d4a984a6
Filipe <filipe.m.mir@gmail.com>
2016-04-07 21:26:00 -0400

    fleshed out lastfm and genius db

--- genius.rb
+++ genius.rb
@@ -24,25 +24,18 @@ hop_along2 = HTTParty.get(
 
 class Genius
   include HTTParty
+ # debug_output $stdout
 
   base_uri 'https://api.genius.com'
-
-  def get(url, params = {})
-    binding.pry
-    self.class.get(
-      url + '?access_token=' + ENV['GENIUS_TOKEN'],
-      query: params
-    )
-  end
 
   def search(search_terms)
     get('/search', { q: search_terms })
   end
 
-  def artist_id(artist_name)
-    search = search(artist_name)
-    status_code = search['meta']['status']
-    top_result = search['response']['hits'].first
+  def find_artist_id(artist_name)
+    search_results = search(artist_name)
+    status_code = search_results['meta']['status']
+    top_result = search_results['response']['hits'].first
     if status_code == 200
       top_result['result']['primary_artist']['id']
     else
@@ -51,14 +44,29 @@ class Genius
   end
 
   def artist_info(artist_id)
-    get('/artists', { id: artist_id })
+    get('/artists/' + artist_id.to_s)
+  end
+
+  def artist_songs(artist_id)
+    get(
+      '/artists/' + artist_id.to_s + '/songs'
+    )
+  end
+
+  private
+
+  def get(url, params = {})
+    self.class.get(
+      url + '?access_token=' + ENV['GENIUS_TOKEN'],
+      query: params
+    )
   end
 
 end
 
 genius = Genius.new
-hop_along_id = genius.artist_id('hop along')
-hop_along = genius.artist_info(hop_along_id) # Does not work
-genius.get('/artists/37695') # Works
+# hop_along_id = genius.find_artist_id('hop along')
+# hop_along = genius.artist_info(hop_along_id) # Does not work
+# genius.get('/artists/37695') # Works
  
 binding.pry
