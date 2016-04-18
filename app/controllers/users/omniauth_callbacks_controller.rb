f3a7909bb8e8e0ce7f31da8db16030a7cceb21b3
Filipe <filipe.m.mir@gmail.com>
2016-04-18 11:00:25 -0400

    Added sign out button

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def lastfm
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user
-      flash[:success] = "Hello, #{@user.uid}!"
-    elsif @user.save
+    if @user.persisted? || @user.save
       sign_in_and_redirect @user
       flash[:success] = "Hello, #{@user.uid}!"
     else
-      flash[:error] = "Oops. We couldn't log you in."
+      flash[:error] = "Oops. Login failed."
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to root_path
     end
-    binding.pry
   end
 end
