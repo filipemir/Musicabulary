9b36cbaf2160eb9c6a9e46a3e556d594965536c5
Filipe <filipe.m.mir@gmail.com>
2016-04-19 11:34:55 -0400

    Added badges to read

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require_relative 'support/vcr'
 require_relative 'support/omniauth_macro'
 
+require 'coveralls'
+Coveralls.wear!('rails')
+
 RSpec.configure do |config|  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
