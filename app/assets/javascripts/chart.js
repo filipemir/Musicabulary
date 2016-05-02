d266d1d0ccef95a7bf8922c4a9fe7ed5493b0d2e
Filipe <filipe.m.mir@gmail.com>
2016-05-02 12:38:16 -0400

    Added description of methodology and sources

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -192,7 +192,7 @@ function updateScaleNumber(d, i) {
   var x = xScale.domain()[0] + (max - min) * (i + 1) / dividerNum;
   var scaledX = xScale(x);
   var roundedX = Math.round(x);
-  scaleNumber.transition().duration(500)
+  scaleNumber.transition().duration(2000)
     .tween(this.textContent, function() {
       var i = d3.interpolate(oldX, roundedX);
       return function(t) {
@@ -231,32 +231,3 @@ bubbles.data(dataset)
   .each(setInitialPosition)
   .each(attachPopUp)
   .each(setWordiness)
-
-
-// function resize() {
-//     var width = parseInt(d3.select("#graph").style("width")) - margin * 2;
-//     var height = parseInt(d3.select("#graph").style("height")) - margin * 2;
-
-//     /* Update the range of the scale with new width/height */
-//     xScale.range([0, width]).nice(d3.time.year);
-//     yScale.range([height, 0]).nice();
-
-//      Update the axis with the new scale 
-//     graph.select('.x.axis')
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(xAxis);
-
-//     graph.select('.y.axis')
-//       .call(yAxis);
-
-//     /* Force D3 to recalculate and update the line */
-//     graph.selectAll('.line')
-//       .attr("d", line);
-//   }
-
-//   d3.select(window).on('resize', resize); 
-
-//   resize();
-// });
-
-
